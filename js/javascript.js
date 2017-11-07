0227da80627f2d4267222d24a2375676d9c954b1
Asaad Khattab <asaad1592@gmail.com>
2017-11-06 20:53:49 -0500

    Added a readme file and another option for my carousel

--- js/javascript.js
+++ js/javascript.js
@@ -1,37 +1,12 @@
+
 /* SLICK CAROUSEL JQUERY PLUGIN */
 $(document).ready(function(){
   $('.carousel').slick({
     fade: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    /*autoplay: true,*/
+    /* autoplaySpeed: 3000,*/
     pauseOnHover:true,
     dots: true,
-  });
-});
-
-/* ANIMSITION PLUGIN */
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'fade-in-right',
-    outClass: 'fade-out-right',
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
   });
 });
 
